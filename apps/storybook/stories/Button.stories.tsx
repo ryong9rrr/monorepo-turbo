d25b1995d34d5e47cf76a79476d3efbdfcff90f9
yong <tkddbs1644@gmail.com>
2023-12-20 00:17:55 +0900

    config: set config @repo/storybook

---
+++ apps/storybook/stories/Button.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@repo/ui';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: props => (
+    <Button {...props} appName="Turborepo">
+      Hello
+    </Button>
+  ),
+  name: 'Button',
+  args: {
+    children: 'Hello',
+  },
+};
