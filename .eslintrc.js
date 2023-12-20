1e74980c95a8506a602b33a6bc8efc0192e95b6f
yong <tkddbs1644@gmail.com>
2023-12-21 06:58:07 +0900

    config: set prettierignore & eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  root: true,
   ignorePatterns: ['apps/**', 'packages/**'],
   extends: ['@repo/eslint-config/base.js'],
   parserOptions: {
