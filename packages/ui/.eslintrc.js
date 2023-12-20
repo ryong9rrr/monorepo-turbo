401b9015681f560130280817024edc8ae13a4a7e
yong <tkddbs1644@gmail.com>
2023-12-21 08:04:57 +0900

    config(apps/webapp): create new vite project

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,6 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
   extends: ['@repo/eslint-config/storybook.js', '@repo/eslint-config/react.js'],
   ignorePatterns: ['storybook-static'],
 };
